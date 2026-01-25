cfbf35566be790ab95ec001ea841f013ab1ba4c3
NicolasCHANTEUX <166408814+NicolasCHANTEUX@users.noreply.github.com>
2026-01-25 22:48:37 +0100

    sans les lignes en index

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->safeLoad();
-
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/Core/helpers.php';
 
