3d320c0fc1f9519fab3671304544b8472492cbfd
NicolasCHANTEUX <166408814+NicolasCHANTEUX@users.noreply.github.com>
2026-01-25 22:39:47 +0100

    correction config

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->safeLoad();
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/Core/helpers.php';
 
